cc17525c28a8381cea3d3d3d437b45074922a0ed
sc-mitton <scm.mitton@gmail.com>
2025-01-07 16:24:23 -0700

    fix: module resolution errors

--- src/AnimatedNumbers.tsx
+++ src/AnimatedNumbers.tsx
@@ -41,25 +41,4 @@ const AnimatedNumbers = (props: AnimatedNumbersProps) => {
   );
 };
 
-/*
-Basic logic:
-
-When a new value comes in:
-
-1. If there is an animation currently in progress then queue the new value with any prefix prepended
-
-2. For an animation cycle, split the new number and set the new number state
-
-3. Loop through the new number and old number and compare digits, determing which way the new numbers
-and old numers need to animate. There are two versions of the number, one that's visible, and one outside
-the visible clipping container. If a slot has the same number between the old and new nummer, it wont be animated.
-
-4. Set the new positions wich will trigger the animations of the slots
-
-5. At the end, clear the new number and set the old number to the new number
-
-6. Pop any queued values and set the formated value, which will retriger the animation cycle
-
-*/
-
 export default AnimatedNumbers;
